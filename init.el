17d418727e6a158583ee33ffbde4930ced157a21
Felix Z. Hoffmann <felix11h.dev@gmail.com>
2018-01-07 13:24:31 +0100

    org-mode LaTeX export specifications
    hyperref setup, geometry

--- init.el
+++ init.el
@@ -642,6 +642,12 @@ langtool-disabled-rules '("WHITESPACE_RULE")
 
 (require 'wc-mode)
 
+;; ~~~~~~~~~~~~~~~~~~~~~~~~~~ Word-count ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+(require 'mediawiki)
+
+
+
 ;; ~~~~~~~~~~~~~~~~~~~~ current date yyyy-mm-dd ~~~~~~~~~~~~~~~~~~~~~~~
 
 (defun insert-current-date () (interactive)
@@ -720,3 +726,33 @@ langtool-disabled-rules '("WHITESPACE_RULE")
 
 (bind-key* "C-c k" (lambda() (interactive)(find-file "~/admin/events/Wikimedia_Open_Science_fellow_2017/wikimedia_fellow_2017_main.org")))
 (bind-key* "C-c 9" (lambda() (interactive)(find-file "/ssh:hoffmann@fias.uni-frankfurt.de:tramp.org")))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-latex-default-packages-alist
+   (quote
+    (("AUTO" "inputenc" t)
+     ("T1" "fontenc" t)
+     ("" "fixltx2e" nil)
+     ("" "graphicx" t)
+     ("" "longtable" nil)
+     ("" "float" nil)
+     ("" "wrapfig" nil)
+     ("" "rotating" nil)
+     ("normalem" "ulem" t)
+     ("" "amsmath" t)
+     ("" "textcomp" t)
+     ("" "marvosym" t)
+     ("" "wasysym" t)
+     ("" "amssymb" t)
+     ("colorlinks=true, linkcolor=blue, urlcolor=blue, citecolor=blue" "hyperref" t)
+     "\\tolerance=1000"
+     ("left=1.65in, right=1.65in, top=1.25in, bottom=1.5in" "geometry" t)))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
