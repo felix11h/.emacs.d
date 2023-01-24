76ce54229a9324efa898f3d87263cfe5c17caa81
Felix Hoffmann <felix11h.dev@gmail.com>
2023-01-24 19:08:30 +0000

    change backup file behaviour

--- init.el
+++ init.el
@@ -110,6 +110,15 @@
 (setq initial-scratch-message nil)              ;; suppress message in *scratch*
 (setq same-window-regexps '("."))               ;; never split windows
 
+
+;; Write backups to ~/.emacs.d/backup/
+(setq backup-directory-alist '(("." . "~/.emacs.d/backup"))
+      backup-by-copying      t  ; Don't de-link hard links
+      version-control        t  ; Use version numbers on backups
+      delete-old-versions    t  ; Automatically delete excess backups:
+      kept-new-versions      100 ; how many of the newest versions to keep
+      kept-old-versions      25) ; and how many of the old
+
 ;; ---------------    load paths    ---------------
 
 ;; adds load paths of subdirs in .emacs.d/modes
